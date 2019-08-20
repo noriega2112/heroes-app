cdc50dc23d3a30ea07ec7b4442d31c7a895568fa
Edwin Noriega <noriegaedwin18@outlook.com>
2019-08-19 21:25:57 -0600

    first commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from "../../services/heroes.service";
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[] = [];
+
+  constructor ( private _heroesService:HeroesService,
+                private _router:Router) {
+
+   }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes); 
+  }
+
+  verHeroe(i:number){
+    this._router.navigate(['heroe', i]);
+  }
+
+}
